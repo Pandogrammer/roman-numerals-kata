0400f54aee688ceaf4f4ef864a7b5d77e8d9b3d1
Fernando Hermes <fernando.hermes@etermax.com>
2020-09-10 22:23:04 -0300

    Si es un numero conocido se imprime directamente

--- src/test/kotlin/RomanNumeralsTest.kt
+++ src/test/kotlin/RomanNumeralsTest.kt
@@ -21,7 +21,21 @@ class RomanNumeralsTest {
         assertEquals(knownNumbers, romanConverter.knownNumbers)
     }
 
+    @Test
+    fun `si es un numero conocido se imprime directamente`(){
+        val numbers = knownNumbers.keys
+
+        numbers.forEach {
+            val result = romanConverter.convert(it)
+
+            assertEquals(knownNumbers[it], result)
+        }
+    }
 }
 
 class RomanConverter(val knownNumbers: Map<Int, String>) {
+    fun convert(number: Int): String {
+        return knownNumbers[number]!!
+    }
+
 }
