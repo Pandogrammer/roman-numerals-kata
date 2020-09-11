32b3b3010301628790a1c2861b2a5af4a23f1d31
Fernando Hermes <fernando.hermes@etermax.com>
2020-09-10 22:28:31 -0300

    Puedo generar numeros desconocidos sumando numeros conocidos

--- src/test/kotlin/RomanNumeralsTest.kt
+++ src/test/kotlin/RomanNumeralsTest.kt
@@ -1,5 +1,4 @@
 import junit.framework.Assert.assertEquals
-import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 
@@ -13,16 +12,17 @@ class RomanNumeralsTest {
     )
 
     @Before
-    fun setUp(){
+    fun setUp() {
         romanConverter = RomanConverter(knownNumbers)
     }
+
     @Test
-    fun `numeros conocidos`(){
+    fun `numeros conocidos`() {
         assertEquals(knownNumbers, romanConverter.knownNumbers)
     }
 
     @Test
-    fun `si es un numero conocido se imprime directamente`(){
+    fun `si es un numero conocido se imprime directamente`() {
         val numbers = knownNumbers.keys
 
         numbers.forEach {
@@ -31,11 +31,37 @@ class RomanNumeralsTest {
             assertEquals(knownNumbers[it], result)
         }
     }
+
+    @Test
+    fun `puedo generar numeros desconocidos sumando numeros conocidos`() {
+        val numbers = mapOf(2 to "II", 3 to "III")
+
+        numbers.keys.forEach {
+            val result = romanConverter.convert(it)
+
+            assertEquals(numbers[it], result)
+        }
+    }
 }
 
 class RomanConverter(val knownNumbers: Map<Int, String>) {
-    fun convert(number: Int): String {
-        return knownNumbers[number]!!
+    fun convert(number: Int): String? {
+        if (knownNumber(number))
+            return convertKnownNumber(number)
+
+        var sum = 0
+        var result = ""
+        while(sum < number){
+            val n = 1
+            result += convertKnownNumber(n)
+            sum += n
+        }
+
+        return result
     }
+
+    private fun convertKnownNumber(number: Int) = knownNumbers.getValue(number)
+
+    private fun knownNumber(number: Int) = knownNumbers.containsKey(number)
 
 }
